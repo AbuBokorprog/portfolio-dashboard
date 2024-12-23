36efed44222602fbba8da9f0cee58dbc41652762
AbuBokorprog <abubokor1066@gmail.com>
2024-12-24 01:10:07 +0600

    add custom query.

--- src/redux/features/baseApi.js
+++ src/redux/features/baseApi.js
@@ -1,10 +1,29 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://pokeapi.co/api/v2/',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const customBaseQuery = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  return result;
+};
 
 export const baseApi = createApi({
-  reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
-  tagTypes: ["products", "cart", "order"],
+  reducerPath: 'api',
+  baseQuery: customBaseQuery,
+  tagTypes: ['products', 'cart', 'order'],
   endpoints: () => ({}),
 });
 
